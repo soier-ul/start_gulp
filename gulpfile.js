c216fc2aaf1d2842491c80a5163871bfb2960f10
soier-ul <ulanovus@gmail.com>
2023-11-30 14:20:09 +0600

    fixed govno

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
+    // 'node_modules/jquery/dist/jquery.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
